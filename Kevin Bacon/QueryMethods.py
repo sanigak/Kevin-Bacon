45e12d9957de577be554825dedd0144fecde6787
Keith Saniga <sanigak@outlook.com>
2019-02-28 18:59:27 -0500

    engine mods

--- Kevin Bacon/QueryMethods.py
+++ Kevin Bacon/QueryMethods.py
@@ -9,6 +9,7 @@ mydb = myclient["KevinBacon"]
 col = mydb["MoviesCOPY"]
 
 solution = ""
+stillWorking = True
 
 def moviesGivenActor(actor):
     outputList = []
@@ -24,23 +25,46 @@ def moviesGivenActor(actor):
 
 def castGivenMovie(movie):
     outputList = []
+    finalList = []
     query = col.find({"title":movie},{"cast":1, "_id":0})
     for item in query:
         cast = item["cast"]
         outputList.append(cast)
+    for item in outputList[0]:
+        stringy = str(item)
+        finalList.append(stringy)
    
-    return outputList[0]
+    return finalList
 
 def Engine(actor, solution):
 
     movies = moviesGivenActor(actor)
+    actorTEMP = ""
+
+    levelOfActors = []
 
     for movie in movies:
-        print(movie)
+        
         cast = castGivenMovie(movie)
         for subactor in cast:
-            print(subactor)
+            interator = 0
+
             if subactor == "Kevin Bacon":
-                solution += actor + " was in " + movie + " with Kevin Bacon"
+                solution += (subactor + " was in " + movie + " with Kevin Bacon")
+                global stillWorking
+                stillWorking = False
+                print(solution)
                 return solution
-    Engine(subactor, solution)
+            else:
+                levelOfActors.append(subactor)
+
+    levelOfActors = set(levelOfActors)
+    levelOfActors = list(levelOfActors)
+
+
+    return levelOfActors
+        
+
+
+solutiony = Engine("Donald Trump", solution)
+
